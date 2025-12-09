01906b1ea5211c9654e701c0d39388c784189f59
Claudio Morel <claudio.morel@ug.uchile.cl>
2025-12-09 12:04:50 -0300

    testeando el despliegue

--- app/app.py
+++ app/app.py
@@ -5,7 +5,7 @@ app = FastAPI()
 
 @app.get("/")
 def root():
-    return {"mensaje": "API funcionando"}
+    return {"mensaje": "el despliegue fue exitoso"}
 
 @app.get("/suma/{a}/{b}")
 def calcular(a: int, b: int):
